a2262143e999aa77073b19efbc66d60c1619bc5e
oleksandr.cheliadin <oleksandr.cheliadin@4irelabs.com>
2025-03-20 10:02:50 +0200

    fix response

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
-  res.json("ipfs://QmSUMUHbMxHVksmzPTo8XqG5anDFs8sZ5cxz4ajfDUT14y");
+  res.json({
+    name: "CARBONCOIN Certificate #9",
+    description: "Carbon Offset Project",
+    tokenAddress: "0xF6636D8e4d22715124Cb5a02703dA0827833ad30",
+    txHash:
+      "https://amoy.polygonscan.com/tx/0x9f4fe5d9f78d2a5293082d5157a08a1ee09da3be39fa13eb6ec44b12ea771bb0",
+    image: "ipfs://Qmbr67MgmCBSRbXJoCsNZ2oeznMXwwqxEpfKbWHeueNPb3",
+  });
 });
 
 app.listen(PORT, () => {
