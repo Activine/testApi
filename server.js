62bd43acf9f60cfb094662b8d739bb4d8440a8a7
oleksandr.cheliadin <oleksandr.cheliadin@4irelabs.com>
2025-03-20 09:30:36 +0200

    fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
-  res.json({ ipfs: "ipfs://QmSUMUHbMxHVksmzPTo8XqG5anDFs8sZ5cxz4ajfDUT14y" });
+  res.json("ipfs://QmSUMUHbMxHVksmzPTo8XqG5anDFs8sZ5cxz4ajfDUT14y");
 });
 
 app.listen(PORT, () => {
