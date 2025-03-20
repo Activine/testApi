17fa732d5cf04361d1bd07149949b9ab249237ae
oleksandr.cheliadin <oleksandr.cheliadin@4irelabs.com>
2025-03-20 09:17:38 +0200

    fix

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const express = require("express");
-const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 4000;
-
-// Разрешаем CORS (по необходимости)
-app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({ ipfs: "ipfs://QmSUMUHbMxHVksmzPTo8XqG5anDFs8sZ5cxz4ajfDUT14y" });
